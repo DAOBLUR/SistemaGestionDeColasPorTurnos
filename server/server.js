ac292155b3a944dc10b9352530d0f8504bd9fc30
DAOBLUR <102988549+DAOBLUR@users.noreply.github.com>
2024-09-12 13:07:15 -0500

    Jenkinsfile

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 
 const publicPath = path.resolve(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // * =============================
 // *  Habilitar la carpeta public
